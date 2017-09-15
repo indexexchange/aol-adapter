dfda5d4ddb1c654a05a8da862a6a1072cbc34546
Zhukovsky <valentin.zhukovsky@oath.com>
2017-09-15 12:01:41 +0300

    Refactored unit tests for generateRequestObj.

--- spec/generateRequestObj.spec.js
+++ spec/generateRequestObj.spec.js
@@ -75,39 +75,20 @@ describe('generateRequestObj', function () {
     var returnParcels;
     var requestObject;
 
-    describe('should return a correctly formated object', function () {
-
+    function assertRequestsForPartnerConfig(partnerConfig, assert) {
         /* Instatiate your partner module */
-        partnerInstance = partnerModule(oneDisplayConfigs.na);
-        partnerProfile = partnerInstance.__profile;
+		partnerInstance = partnerModule(partnerConfig);
+		partnerProfile = partnerInstance.__profile;
 
         /* Generate a request object using generated mock return parcels. */
-        returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
+		returnParcels = generateReturnParcels(partnerProfile, partnerConfig);
 
-        for (var i = 0; i < returnParcels.length; i++) {
-            requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
+		returnParcels.forEach((item) => {
+		    requestObject = partnerInstance.__generateRequestObj([item]);
 
-            /* Simple type checking, should always pass */
-            it('should contain the correct properties', function () {
-                var result = inspector.validate({
-                    type: 'object',
-                    strict: true,
-                    properties: {
-                        url: {
-                            type: 'string',
-                            minLength: 1
-                        },
-                        callbackId: {
-                            type: 'string',
-                            minLength: 1
-                        }
-                    }
-                }, requestObject);
-
-                expect(result.valid).to.be.true;
-            });
-        }
-    });
+		    assert(requestObject);
+        });
+	}
 
     /* Test that the generateRequestObj function creates the correct object by building a URL
      * from the results. This is the bid request url that wrapper will send out to get demand
@@ -116,167 +97,93 @@ describe('generateRequestObj', function () {
      * The url should contain all the necessary parameters for all of the request parcels
      * passed into the function.
      */
-
     describe('should correctly build onedisplay endpoint url', function () {
         var url, i, match;
 
+		it('should return a correctly formated objects', function () {
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, (requestObject) => {
+				var result = inspector.validate({
+					type: 'object',
+					strict: true,
+					properties: {
+						url: {
+							type: 'string',
+							minLength: 1
+						},
+						callbackId: {
+							type: 'string',
+							minLength: 1
+						}
+					}
+				}, requestObject);
+
+				expect(result.valid).to.be.true;
+			})
+		});
+
         it('should correctly set NA url', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                expect(url.match("adserver-us.adtech.advertising.com", "url is incorrect").length).to.equal(1);
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				expect(url.match('adserver-us.adtech.advertising.com', 'url is incorrect').length).to.equal(1);
+            });
+        });
 
         it('should correctly set EU url', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.eu);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.eu);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                expect(url.match("adserver-eu.adtech.advertising.com", "url is incorrect").length).to.equal(1);
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.eu, ({url}) => {
+				expect(url.match('adserver-eu.adtech.advertising.com', 'url is incorrect').length).to.equal(1);
+			});
+        });
 
         it('should correctly set ASIA url', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.asia);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.asia);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                expect(url.match("adserver-as.adtech.advertising.com").length, "url is incorrect").to.equal(1);
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.asia, ({url}) => {
+				expect(url.match('adserver-as.adtech.advertising.com').length, 'url is incorrect').to.equal(1);
+			});
+        });
 
         it('should correctly set CMD request paramater', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                match = url.match(/;cmd=(.*?);/);
-                expect(match[1], "cmd is incorrect or not present").to.equal('bid');
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				match = url.match(/;cmd=(.*?);/);
+				expect(match[1], 'cmd is incorrect or not present').to.equal('bid');
+			});
+        });
 
         it('should correctly set CORS request paramater', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                match = url.match(/;cors=(.*?);/);
-                expect(match[1], "CORS is incorrect or not present").to.equal('yes');
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				match = url.match(/;cors=(.*?);/);
+				expect(match[1], 'CORS is incorrect or not present').to.equal('yes');
+			});
+        });
 
         it('should correctly set V request paramater', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                match = url.match(/;v=(.*?);/);
-                expect(match[1], "V is incorrect or not present").to.equal('2');
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				match = url.match(/;v=(.*?);/);
+				expect(match[1], "V is incorrect or not present").to.equal('2');
+			});
+        });
 
         it('should correctly set MISC request paramater', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                match = url.match(/;misc=(.*?);/);
-                expect(match[1], "V is incorrect or not present").to.be.not.null;
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				match = url.match(/;misc=(.*?);/);
+				expect(match[1], 'V is incorrect or not present').to.be.not.null;
+			});
+        });
 
         it('should correctly set unique callback request parameter for each request', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                match = url.match(/;callback=(.*?);/);
-                expect(match[1], "callback function is incorrect").to.equal('window.headertag.' + partnerProfile.namespace + '.adResponseCallbacks.' + requestObject.callbackId);
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				match = url.match(/;callback=(.*?);/);
+				expect(match[1], 'callback function is incorrect').to.equal('window.headertag.' + partnerProfile.namespace + '.adResponseCallbacks.' + requestObject.callbackId);
+			});
+        });
 
         it('should correctly set networkId', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                expect(url.match("9959.1").length, "networkId is incorrect").to.equal(1);
-            }
-        })
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				expect(url.match('9959.1').length, "networkId is incorrect").to.equal(1);
+			});
+        });
 
         it('should correctly set placementId request parameter for each request/slot', function () {
-            /* Instatiate your partner module */
-            partnerInstance = partnerModule(oneDisplayConfigs.na);
-            partnerProfile = partnerInstance.__profile;
-
-            /* Generate a request object using generated mock return parcels. */
-            returnParcels = generateReturnParcels(partnerProfile, oneDisplayConfigs.na);
-
-            for (i = 0; i < returnParcels.length; i++) {
-                requestObject = partnerInstance.__generateRequestObj([returnParcels[i]]);
-                url = requestObject.url;
-                expect(url.match(returnParcels[i].xSlotRef.placementId).length, "placementId is incorrect").to.equal(1);
-            }
-        })
-
-        /* ---------- ADD MORE TEST CASES TO TEST CASES FOR EVERY NEW CHANGE/FEATURE ------------*/
-
+			assertRequestsForPartnerConfig(oneDisplayConfigs.na, ({url}) => {
+				expect(url.match(returnParcels[i].xSlotRef.placementId).length, "placementId is incorrect").to.equal(1);
+			});
+        });
     });
-    /* -----------------------------------------------------------------------*/
 });
